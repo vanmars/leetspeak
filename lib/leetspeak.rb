b79cdb067ca3bebd9fdc36b89bc3711ce35ab7f8
Alex Goodwin <hagoodwi@gmail.com>
2020-09-21 14:21:26 -0700

    add passing test to replace letter e

--- lib/leetspeak.rb
+++ lib/leetspeak.rb
@@ -1,10 +1,12 @@
-def leetspeak
-  # for the entire string, split into words
-  split_sentence = leetspeak.split
-  split_sentence.each do |word|
-    if (word.include? "e")
-      
+class String
+  def leetspeak
+    split_sentence = self.split()
+    index = -1
+    split_sentence.each do |word|
+      index += 1
+      split_sentence[index] = word.gsub("e","3")
+    end
+    return split_sentence.join(" ")
   end
-  # check if string contains e, replace with 3
-  
 end
+
--- spec/leetspeak_spec.rb
+++ spec/leetspeak_spec.rb
@@ -1,9 +1,15 @@
 require ('leetspeak')
 require ('rspec')
 
-describe ('leetspeak') do
-  it("replaces the letter "e" with "3"") do
-    expect(leetspeak("epicodus")).to(eq("3picodus"))
+# describe ('#leetspeak') do
+#   it("replace the letter e with 3") do
+#     expect(leetspeak("epicodus")).to(eq("3picodus"))
+#   end
+# end
+
+describe ('String#leetspeak') do
+  it("replaces the letter e with 3") do
+    expect("epicodus".leetspeak).to(eq("3picodus"))
   end
 end
 
