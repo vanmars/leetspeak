b77b0f35ea8e12e0dd13cd5c073c2a2db3c059a4
Alex Goodwin <hagoodwi@gmail.com>
2020-09-21 15:08:02 -0700

    Write and pass test 2

--- spec/leetspeak_spec.rb
+++ spec/leetspeak_spec.rb
@@ -8,8 +8,12 @@ require ('rspec')
 # end
 
 describe ('String#leetspeak') do
-  it("replaces the letter e with 3") do
-    expect("epicodus".leetspeak).to(eq("3picodus"))
+  # it("replaces the letter e with 3") do
+  #   expect("epicodus".leetspeak).to(eq("3picodus"))
+  # end
+  it("replaces the letter o with 0") do
+    expect("epicodus".leetspeak).to(eq("3pic0dus"))
   end
+
 end
 
