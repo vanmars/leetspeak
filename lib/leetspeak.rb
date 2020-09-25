26f7d4ac0bfb725ae819777543d28b85e9fb847a
Vanessa Stewart <vstewart1213@gmail.com>
2020-09-24 18:17:13 -0700

    Add and PASS  additional test to check all rules and refactor leetscpeak.rb

--- lib/leetspeak.rb
+++ lib/leetspeak.rb
@@ -1,7 +1,7 @@
 class String
   def leetspeak
     new_string = self.gsub(/[EeoOI]/, 'E' => 3, 'e' => 3, 'o' => 0, 'O' => 0, 'I' => 1)
-    new_string.gsub!(/(?<=\S)[s]/i,"z")
+    new_string.gsub!(/(?<=\S)s/i,"z")
     return new_string
   end
 end
--- spec/leetspeak_spec.rb
+++ spec/leetspeak_spec.rb
@@ -11,11 +11,14 @@ describe ('String#leetspeak') do
   # it("replaces the capital letter I with 1, but not the lower case i") do
   #   expect("who am I? epicodus".leetspeak).to(eq("wh0 am 1? 3pic0dus"))
   # end
-  # it('replaces every "s" in a string with a "z"') do
-  #   expect("epicodus".leetspeak).to(eq("3pic0duz"))
-  # end
+  it('replaces every "s" in a string with a "z"') do
+    expect("epicodus".leetspeak).to(eq("3pic0duz"))
+  end
   it('replaces every "s" in a string with a "z", unless the "s" begins the word') do
     expect("smart".leetspeak).to(eq("smart"))
+  end
+  it('applies all rules') do
+    expect("I seconds".leetspeak).to(eq("1 s3c0ndz"))
   end
 
 end
